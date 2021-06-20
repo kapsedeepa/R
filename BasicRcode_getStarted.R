8ed96e28478acb8409d732ab863da7e00083503a
kapsedeepa <52926633+kapsedeepa@users.noreply.github.com>
2021-06-20 16:31:35 -0400

    Update BasicRcode_getStarted.R

--- BasicRcode_getStarted.R
+++ BasicRcode_getStarted.R
@@ -26,15 +26,12 @@ number_of_records <- nrow(iris)
 
 
 #  Below code calculates & print the result of raising number_of_records to the 1/2 power.
-# number_of_records^(power) calculates  to the power where ^ is the exponent arithmetic R operator.
-# Using <- right to left assignment operator to assign the value to variable result_of_raising_n
-# and cat() to print an informative message.
 
 result_of_raising_n <- number_of_records^(1/2)
 cat("The result of raising n(number of records) to the 1/2 power is",result_of_raising_n,"\n")
 
 
-# Using diamonds$price notation retrieves only Sepal column values of iris dataset
+# Using iris$Petal.Width notation retrieves only Petal.Width column values of iris dataset
 # na.rm=TRUE will remove NA values for the column price of iris dataset.
 
 # Using min(), mean(), median() and interquartile() function
@@ -43,9 +40,7 @@ PetalWidth_min_value <- min(iris$Petal.Width,na.rm=TRUE)
 
 PetalWidth_mean_value <- mean(iris$Petal.Width,na.rm=TRUE)
 
-
 PetalWidth_median_value <- median(iris$Petal.Width,na.rm=TRUE)
-
 
 PetalWidth_interquartile_value <- IQR(iris$Petal.Width,na.rm=TRUE)
 
@@ -56,6 +51,38 @@ cat("The statistics iris petal width are \n", "mean  is ", PetalWidth_mean_value
     "median  is ",PetalWidth_median_value, "\n",
     "minimum is ",PetalWidth_min_value,"\n",
     "interquartile  is ",PetalWidth_interquartile_value,"\n")
+
+
+# Know your data well head(), tail(), str()
+#    head(iris)         # returns first six rows by default
+#    tail(iris, n = 4)  # last 4 rows
+#    str(iris)          # structure synopsis
+
+head(iris)
+tail(iris)
+str(iris)
+
+# Using factor()  to create ordered vector
+# Customer rating on the scale of 1 to 10 
+
+customer_rating <- c(9, 3, 7, 2, 9, 4, 5, 6, 7, 2, 3, 9)
+
+# Create ordered factor vector using factor() function
+factor.customer_rating <- factor(customer_rating, ordered=TRUE)
+str(factor.customer_rating)
+table(factor.customer_rating)
+
+
+# An ordered factor vector can be indexed by value using a comparison operator
+satisfactory.rating  <- factor.customer_rating[factor.customer_rating > "5" ]
+str(satisfactory.rating)
+table(satisfactory.rating)
+
+# The following code resets the levels of satisfactory.rating to only those with positive counts:
+satisfactory.rating <- factor(satisfactory.rating)
+str(satisfactory.rating)
+table(satisfactory.rating)
+
 
 
 
